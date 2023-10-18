62764be0dece0a41e6094bcebdde3714cff07ec8
Philipp Windischhofer <windischhofer@uchicago.edu>
2023-10-18 17:11:01 -0500

    need to load intel module for tbb

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 NUPHASE_INSTALL_DIR=/home/windischhofer/ARA/nuphaseroot_install
+export ROOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 source /project/avieregg/software/midway3-setup.sh
 module load cmake
 module load ROOT
 module load python
+module load intel
 export LD_LIBRARY_PATH=$NUPHASE_INSTALL_DIR/lib:$LD_LIBRARY_PATH
