ba9b1e507be36d46f1efd975d6c86bf38635a7f1
Philipp Windischhofer <windischhofer@uchicago.edu>
2023-10-18 21:12:12 -0500

    script to combine averages

---
+++ combine_correlators.py
@@ -0,0 +1,38 @@
+import os, argparse, pickle
+import numpy as np
+
+def combine_correlators(outpath, inpaths):
+
+    combined_correlators = {}
+
+    for cur_inpath in inpaths:
+        with open(cur_inpath, 'rb') as cur_infile:
+            cur_data = pickle.load(cur_infile)
+            
+            for pairing_name, pairing_correlator in cur_data.items():
+                if pairing_name not in combined_correlators:
+                    combined_correlators[pairing_name] = {}                    
+
+                indict = cur_data[pairing_name]
+                outdict = combined_correlators[pairing_name]
+                
+                if "num_corr" not in outdict:
+                    outdict["num_corr"] = indict["num_corr"]
+                    outdict["mean"] = indict["mean"]
+                    outdict["var"] = indict["var"]
+                else:
+                    outdict["num_corr"] += indict["num_corr"]
+                    outdict["mean"] += indict["mean"]
+                    outdict["var"] += indict["var"]
+
+    with open(outpath, 'wb') as outfile:
+        pickle.dump(combined_correlators, outfile)
+                    
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--infiles", action = "store", dest = "inpaths", nargs = "+")
+    parser.add_argument("--outfile", action = "store", dest = "outpath")
+    args = vars(parser.parse_args())
+
+    combine_correlators(**args)
