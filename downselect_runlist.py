5bafad4ee819f0f8a2c23136fb747b156b88bc3b
Philipp Windischhofer <windischhofer@uchicago.edu>
2023-10-25 10:52:46 -0500

    add crude night/day selection

--- downselect_runlist.py
+++ downselect_runlist.py
@@ -1,12 +1,20 @@
 import os, argparse, datetime
 import pandas as pd
 
-def downselect_runlist(inpath, veto_path, outpath, year_to_select, buflen_to_select):
+def downselect_runlist(inpath, veto_path, outpath, year_to_select, buflen_to_select, only_night, only_day):
     df_all = pd.read_csv(inpath)
     runs_vetoed = set(pd.read_csv(veto_path)["run"])
 
-    selector = df_all.apply(lambda row: datetime.datetime.fromisoformat(row["readout_time"]).year == year_to_select and row["buffer_length"] == buflen_to_select,
-                            axis = 1)
+    if only_night:
+        def cutter(row):
+            cur_dt = datetime.datetime.fromisoformat(row["readout_time"])
+            return (cur_dt.year == year_to_select) and (cur_dt.month >= 5) and (cur_dt.month <= 8) and (row["buffer_length"] == buflen_to_select)        
+    elif only_day:
+        raise RuntimeError("Error: not implemented yet")
+    else:
+        cutter = lambda row: (datetime.datetime.fromisoformat(row["readout_time"]).year == year_to_select) and (row["buffer_length"] == buflen_to_select)
+    
+    selector = df_all.apply(cutter, axis = 1)
     runs_selected = set(df_all[selector]["run"])
     runs_selected = list(runs_selected.difference(runs_vetoed))
     
@@ -21,6 +29,8 @@ if __name__ == "__main__":
     parser.add_argument("--vetoed_runs", action = "store", dest = "veto_path")
     parser.add_argument("--out", action = "store", dest = "outpath")
     parser.add_argument("--year", action = "store", dest = "year_to_select", type = int, default = 2019)
+    parser.add_argument("--night", action = "store_true", dest = "only_night", default = False)
+    parser.add_argument("--day", action = "store_true", dest = "only_day", default = False)
     parser.add_argument("--buflen", action = "store", dest = "buflen_to_select", type = int, default = 512)
     args = vars(parser.parse_args())
 
